708774e9ac5861af47c1a4e301fa542a30f0c586
ayk-trsk <1219noppoppo@gmail.com>
2021-05-11 21:36:26 +0000

    [Add] CRUDの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,15 +15,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
-  def edit
-    @user = User.find(params[])
+  def index
+    @user = User.find(current_user.id)
+    @users = User.all
   end
-
+  
+  def show
+    @user = User.find(params[:id])
+    @books = Book.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
