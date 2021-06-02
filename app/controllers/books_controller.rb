572741c98be84886398c6a8e2cd838bf90d35674
ayk-trsk <1219noppoppo@gmail.com>
2021-06-02 05:30:54 +0000

    [Add] いいね/コメント機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
@@ -22,8 +22,10 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
+    @book_comments = @book.book_comments
   end
-  
+
   def edit
     @book = Book.find(params[:id])
     if @book.user == current_user
@@ -32,7 +34,7 @@ class BooksController < ApplicationController
       redirect_to books_path
     end
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -48,7 +50,7 @@ class BooksController < ApplicationController
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
