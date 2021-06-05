bb0e17dba855e2e08d78fe0b1bd1011e1be20295
ayk-trsk <1219noppoppo@gmail.com>
2021-06-05 17:08:03 +0000

    [Update] いいね機能とコメント機能を非同期通信に

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,27 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.build(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comments = @book.book_comments
+    if @book_comment.save
+      render :index
+    end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+    @book_comment = BookComment.find(params[:id])
+    if @book_comment.destroy
+      render :index
+    end
   end
 
   private
 
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment, :user_id, :book_id)
   end
 end
